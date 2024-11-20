1f62eb09f2704c3b6b8d2be8f41198d1e289e0e0
Ricardo Almeida <ricardo2675@proton.me>
2024-11-20 19:48:34 +0000

    Added necessary kotlin and jpa plugins

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,16 @@
+group = "com.rookie"
+version = "0.0.1-SNAPSHOT"
+
 plugins {
     kotlin("jvm") version "1.9.25"
+    kotlin("plugin.jpa") version "2.0.21"
+    kotlin("plugin.noarg") version "2.0.21"
     kotlin("plugin.spring") version "1.9.25"
     id("war")
     id("org.springframework.boot") version "3.3.5"
     id("com.github.ben-manes.versions") version "0.51.0"
     id("io.spring.dependency-management") version "1.1.6"    
 }
-
-group = "com.rookie"
-
-version = "0.0.1-SNAPSHOT"
 
 java { 
     toolchain { 
